9e73b0a61e91e7270850a0ca56c041f58cc108d3
Tinyu <Tinyu.Zhao@gmail.com>
2024-06-07 12:18:52 +0800

    Add button example

---
+++ examples/Basic/button/button.ino
@@ -0,0 +1,68 @@
+/**
+ * @file button.ino
+ * @author tinyu (tinyu@m5stack.com)
+ * @brief M5CoreS3 Touch Test
+ * @version 0.1
+ * @date 2024-06-07
+ *
+ *
+ * @Hardwares: M5CoreS3
+ * @Platform Version: Arduino M5Stack Board Manager v2.0.9
+ * @Dependent Library:
+ * M5GFX: https://github.com/m5stack/M5GFX
+ * M5Unified: https://github.com/m5stack/M5Unified
+ * M5CoreS3: https://github.com/m5stack/M5CoreS3
+ */
+
+#include <M5CoreS3.h>
+
+static m5::touch_state_t prev_state;
+
+void setup(void) {
+    auto cfg = M5.config();
+    CoreS3.begin(cfg);
+
+    CoreS3.Display.setTextColor(RED);
+    CoreS3.Display.setTextDatum(middle_center);
+    CoreS3.Display.setFont(&fonts::Orbitron_Light_24);
+    CoreS3.Display.setTextSize(1);
+
+    CoreS3.Display.drawString("Touch Button Test", CoreS3.Display.width() / 2,
+                              15);
+    CoreS3.Display.fillRect(0, CoreS3.Display.height() - 40,
+                            CoreS3.Display.width() / 3, 40, WHITE);
+    CoreS3.Display.fillRect(CoreS3.Display.width() / 3,
+                            CoreS3.Display.height() - 40,
+                            CoreS3.Display.width() / 3, 40, GREEN);
+    CoreS3.Display.fillRect((CoreS3.Display.width() / 3) * 2,
+                            CoreS3.Display.height() - 40,
+                            CoreS3.Display.width() / 3, 40, YELLOW);
+    CoreS3.Display.drawString("Btn A    Btn B    Btn C",
+                              CoreS3.Display.width() / 2,
+                              CoreS3.Display.height() - 20);
+}
+
+void loop(void) {
+    CoreS3.update();
+    auto touchPoint = CoreS3.Touch.getDetail();
+    if (prev_state != touchPoint.state) {
+        prev_state = touchPoint.state;
+    }
+    if ((CoreS3.Display.height() > touchPoint.y &&
+         touchPoint.y > CoreS3.Display.height() - 40) &&
+        touchPoint.state == m5::touch_state_t::touch_begin) {
+        CoreS3.Display.fillRect(0, 40, CoreS3.Display.width(), 70, BLACK);
+        if (CoreS3.Display.width() / 3 > touchPoint.x && touchPoint.x > 0)
+            CoreS3.Display.drawString("Btn A", CoreS3.Display.width() / 2,
+                                      CoreS3.Display.height() / 2 - 30);
+        if ((CoreS3.Display.width() / 3) * 2 > touchPoint.x &&
+            touchPoint.x > CoreS3.Display.width() / 3)
+            CoreS3.Display.drawString("Btn B", CoreS3.Display.width() / 2,
+                                      CoreS3.Display.height() / 2 - 30);
+        if (CoreS3.Display.width() > touchPoint.x &&
+            touchPoint.x > (CoreS3.Display.width() / 3) * 2)
+            CoreS3.Display.drawString("Btn C", CoreS3.Display.width() / 2,
+                                      CoreS3.Display.height() / 2 - 30);
+        Serial.printf("x:%d ,y:%d", touchPoint.x, touchPoint.y);
+    }
+}
